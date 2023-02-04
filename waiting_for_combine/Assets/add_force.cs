cda4c094c4cdfd0d09625acd89daa0d6e177f5ea
hive <srall123@hotmail.com>
2023-02-05 01:37:01 +0200

    no end, no UI, no start/end scenes

---
+++ waiting_for_combine/Assets/add_force.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class add_force : MonoBehaviour
+{
+	Rigidbody2D rb;
+    Vector2 force;
+    GameObject user_input;
+
+    void modify_physics(Rigidbody2D rb)
+    {
+        if ((Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y)) < 1)
+            rb.drag = 5;
+        else if ((Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y)) < 0.1)
+            rb.drag = 100;
+        else
+            rb.drag = 0.35f;
+        if (Mathf.Abs(rb.angularVelocity) < 20)
+            rb.angularDrag = 10;
+        else
+            rb.angularDrag = 0.35f;
+    }
+
+    void Start()
+    {
+        user_input = GameObject.Find("user_input");
+        Debug.Log("Active the balls!");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        force = user_input.GetComponent<user_input>().firePos.up * user_input.GetComponent<user_input>().currentForce;// this is a Vector provide by Sumin
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        modify_physics(rb);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+}
